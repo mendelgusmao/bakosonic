c02da9b96dc6dad5413d8bbc1c80927dc9d3b4ce
MendelGusmao <mendelsongusmao@gmail.com>
2013-10-22 20:33:34 -0200

    Some fixes

--- bakosonic.ino
+++ bakosonic.ino
@@ -1,29 +1,6 @@
 #include <EEPROM.h>
 #include <SPI.h>
-
-const int SCREEN_STANDBY = 0;
-const int SCREEN_MAIN = 1;
-const int SCREEN_SETUP1 = 2;
-const int SCREEN_SETUP2 = 3;
-
-const int KEY_ONOFF = 0;
-const int KEY_CHAN_UP = 1;
-const int KEY_CHAN_DOWN = 2;
-const int KEY_VOL_UP = 3;
-const int KEY_VOL_DOWN = 4;
-const int KEY_SETUP = 5;
-
-const int PIN_RELAY = 10;
-const int PIN_DIGIPOT = 11; // 11 - CS, 12 - SDI
-
-const int CHAN_FLOOR = 2;
-const int CHAN_CEIL = 83;
-
-const int ADDRESS_TUNING = 0;
-const int ADDRESS_VOLUME = 1;
-const int ADDRESS_BRIGHTNESS = 2;
-const int ADDRESS_CONTRAST = 3;
-const int ADDRESS_VERTICAL = 4;
+#include "const.h"
 
 bool on = false;
 int screen = SCREEN_MAIN;
@@ -88,13 +65,13 @@ void handle_main(int key) {
     break;
 
   case KEY_VOL_UP:
-    if (volume == 255) return
+    if (volume == 255) return;
     volume++;
     set(ADDRESS_VOLUME, volume);
     break;
 
   case KEY_VOL_DOWN:
-    if (volume == 0) return
+    if (volume == 0) return;
     volume--;
     set(ADDRESS_VOLUME, volume);
     break;
@@ -129,7 +106,7 @@ void handle_setup1(int key) {
   }
 }
 
-void handle_setup1(int key) {
+void handle_setup2(int key) {
   switch (key) {
   case KEY_ONOFF:
     toggle_tv();
@@ -148,7 +125,7 @@ void handle_setup1(int key) {
     break;
 
   case KEY_SETUP:
-    change_screen(SCREEN_m);
+    change_screen(SCREEN_SETUP2);
     break;  
   }
 }
@@ -163,10 +140,10 @@ void update_screen() {}
 void toggle_tv() {
   digitalWrite(PIN_RELAY, (on = !on) ? HIGH : LOW);
   set_channel(channel);
-  set_volume(volume);
-  set_brightness(brightness);
-  set_contrast(contrast);
-  set_vertical(vertical);
+  set(ADDRESS_VOLUME, volume);
+  set(ADDRESS_BRIGHTNESS, brightness);
+  set(ADDRESS_CONTRAST, contrast);
+  set(ADDRESS_VERTICAL, vertical);
 }
 
 void set_channel(int new_channel) {
---
+++ const.h
@@ -0,0 +1,23 @@
+const int SCREEN_STANDBY = 0;
+const int SCREEN_MAIN = 1;
+const int SCREEN_SETUP1 = 2;
+const int SCREEN_SETUP2 = 3;
+
+const int KEY_ONOFF = 0;
+const int KEY_CHAN_UP = 1;
+const int KEY_CHAN_DOWN = 2;
+const int KEY_VOL_UP = 3;
+const int KEY_VOL_DOWN = 4;
+const int KEY_SETUP = 5;
+
+const int PIN_RELAY = 10;
+const int PIN_DIGIPOT = 11; // 11 - CS, 12 - SDI
+
+const int CHAN_FLOOR = 2;
+const int CHAN_CEIL = 83;
+
+const int ADDRESS_TUNING = 0;
+const int ADDRESS_VOLUME = 1;
+const int ADDRESS_BRIGHTNESS = 2;
+const int ADDRESS_CONTRAST = 3;
+const int ADDRESS_VERTICAL = 4;
